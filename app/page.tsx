ac2c325275f18201ae874063516eda167c5b5e2d
MadeBy-Herianto <herianto9671@gmail.com>
2025-01-16 02:10:14 +0700

    adjust color for different mode

--- app/page.tsx
+++ app/page.tsx
@@ -13,34 +13,42 @@ export default function Home() {
         {
           name: "HTML",
           link: "programming-languages/html",
+          level: 3,
         },
         {
           name: "CSS",
           link: "programming-languages/css",
+          level: 4,
         },
         {
           name: "Typescript",
           link: "programming-languages/typescript",
+          level: 3,
         },
         {
           name: "Javascript",
           link: "programming-languages/javascript",
+          level: 3,
         },
         {
           name: "Python",
           link: "programming-languages/python",
+          level: 3,
         },
         {
           name: "PHP",
           link: "programming-languages/php",
+          level: 3,
         },
         {
           name: "SQL",
           link: "programming-languages/sql",
+          level: 3,
         },
         {
           name: "R",
           link: "programming-languages/r",
+          level: 3,
         }
       ]
     },
@@ -50,26 +58,32 @@ export default function Home() {
         {
           name: "MVC",
           link: "development-area/mvc",
+          level: 3,
         },
         {
           name: "Front End",
           link: "development-area/front-end",
+          level: 3,
         },
         {
           name:"Back End",
           link: "development-area/back-end",
+          level: 3,
         },
         {
           name:"Database",
           link: "development-area/database",
+          level: 3,
         },
         {
           name:"Mobile",
           link: "development-area/mobile",
+          level: 3,
         },
         {
           name:"Automated Web Testing",
           link: "development-area/automated-testing",
+          level: 3,
         },
       ]
     },
@@ -79,22 +93,27 @@ export default function Home() {
         {
           name: "MySQL",
           link: "/mysql",
+          level: 3,
         },
         {
           name: "PostgreSQL",
           link: "/postgresql",
+          level: 3,
         },
         {
           name: "MongoDB",
           link: "/mongodb",
+          level: 3,
         },
         {
           name: "Redis",
           link: "/redis",
+          level: 3,
         },
         {
           name: "SQLite",
           link: "/sqlite",
+          level: 3,
         },
         {
           name: "GraphQL",
@@ -108,11 +127,13 @@ export default function Home() {
         {
           name: "Windows",
           link: "/windows",
+          level: 3,
         },
         {
           name: "WSL : Windows Subsystem for Linux",
           link: "/wsl",
-        }
+          level: 3,
+        },
         // {
         //   name: "Linux",
         //   link: "/linux",
@@ -129,6 +150,7 @@ export default function Home() {
         {
           name: "VSCode",
           link: "/vscode",
+          level: 3,
         },
         // {
         //   name: "Visual Studio",
@@ -137,6 +159,7 @@ export default function Home() {
         {
           name: "Sublime Text", 
           link: "/sublime-text",
+          level: 3,
         },
         {
           name: "Notepad++",
@@ -154,14 +177,17 @@ export default function Home() {
         {
           name: "Version Control: Git (GitHub, GitLab)",
           link: "/git",
+          level: 3,
         },
         {
           name: "Cloud Hosting: Vercel",
           link: "/vercel",
+          level: 3,
         },
         {
           name: "Cloud Hosting: Firebase",
           link: "/firebase",
+          level: 3,
         },
         // {
         //   name: "Docker",
@@ -176,8 +202,8 @@ export default function Home() {
     }
   ]
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <h1 id="fixed-title" className="fixed text-2xl z-10 md:text-4xl font-[family-name:var(--font-geist-mono)] w-full max-w-5xl top-0 bg-white sm:bg-black sm:text-base  rounded-b-lg border-b border-solid border-[#fff] hidden-title p-4">Freelancer Fullstack</h1>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen pb-20 gap-16 sm:p-16 font-[family-name:var(--font-geist-sans)]">
+        <h1 id="fixed-title" className="fixed text-2xl z-10 md:text-4xl font-[family-name:var(--font-geist-mono)] w-full max-w-5xl top-0 bg-yellow-100 dark:bg-yellow-950 sm:text-base  rounded-b-lg border-b border-solid border-[#fff] hidden-title p-4">Freelancer Fullstack</h1>
         <style jsx>{`
           .hidden-title {
             opacity: 0;
@@ -192,7 +218,7 @@ export default function Home() {
           }
         `}</style>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div className="flex flex-col gap-4 items-center sm:flex-row justify-between w-full align-center border-b border-black sm:border-white">
+        <div className="flex flex-col gap-4 items-center sm:flex-row justify-between w-full align-center border-b border-black dark:border-white">
           <Image 
             className="rounded-full sm:rounded-0 sm:order-1"
             src="/profile.png"
@@ -237,7 +263,7 @@ export default function Home() {
                   <td className="font-[family-name:var(--font-geist-mono)]">herianto@gmail.com</td>
                 </tr>
                 <tr>
-                  <td className="font-[family-name:var(--font-geist-mono)]">GitHub</td>
+                  <td >GitHub</td>
                   <td> : </td>
                   <td className="font-[family-name:var(--font-geist-mono)]">Herianto-S-Mat</td>
                 </tr>
@@ -260,27 +286,28 @@ export default function Home() {
             </table>
           </div>
         </div>
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 max-w-5xl">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 max-w-5xl">
         {
           skills.map((skill, index) => (
 
-            <div key={index} className="bg-gradient-to-br rounded p-4 flex flex-col gap-4 w-full border border-black sm:border-white">
-              <h1 className="text-1xl md:text-2xl font-[family-name:var(--font-geist-mono)] border-b border-black sm:border-white pb-2">{skill.text_about}</h1>
+            <div key={index} className="bg-gradient-to-br rounded p-4 flex flex-col gap-4 w-full border border-black dark:border-white">
+              <h1 className="text-1xl md:text-2xl font-[family-name:var(--font-geist-mono)] border-b border-black dark:border-white pb-2">{skill.text_about}</h1>
               <div className="flex gap-2 sm:gap-4 items-center flex-row flex-wrap justify-center mb-2">
                 {
-                  skill.list.map((item, index) => (
+                  skill.list.map((skill, index) => (
                     <Link
                       key={index}
-                      href={item.link}
-                      className={"rounded-full border border-solid transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-8 sm:h-12 px-2 sm:px-4  dark:bg-opacity-50" + (index % 2 === 0 ? "" : " opacity-50") }
+                      href={skill.link}
+                      className={"rounded-full border border-solid transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-8 sm:h-12 px-2 hover:scale-105 sm:px-4 " + (skill.level === 3 ? "" : " opacity-50") }
                       rel="noopener noreferrer"
+                      translate="no"
                     >
-                      {item.name}
+                      {skill.name}
                     </Link>
                   ))
                 }
               </div>
-              <span className="flex gap-3 items-end flex-wrap justify-end mt-auto">
+              <span className="flex gap-3 items-end flex-wrap justify-end mt-auto border-t border-black dark:border-white">
                 <div className="flex items-center gap-0.5">
                   medium <div className="bg-foreground hover:bg-[#383838] dark:hover:bg-[#ccc] w-4 h-4 tex"/> 
                 </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 
 export default {
+  darkMode: 'media',
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -14,5 +16,22 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    // plugin(function ({ addUtilities }) {
+    //   addUtilities({
+    //     '.text-shadow': {
+    //       'text-shadow': '2px 2px 4px rgba(0, 0, 0, 0.5)',
+    //     },
+    //     '.text-shadow-md': {
+    //       'text-shadow': '3px 3px 6px rgba(0, 0, 0, 0.4)',
+    //     },
+    //     '.text-shadow-lg': {
+    //       'text-shadow': '4px 4px 6px rgba(0, 0, 0, 0.3',
+    //     },
+    //     '.text-shadow-none': {
+    //       'text-shadow': 'none',
+    //     },
+    //   });
+    // }),
+  ]
 } satisfies Config;
